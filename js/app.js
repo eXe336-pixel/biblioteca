7612698702acd1bbf2c8bc222f72a912b5e54579
eXe336 <82899@sistemas.frc.utn.edu.ar>
2024-06-23 01:33:54 -0300

    Buscador de libros funcionando a nivel basico

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const botonBuscar = document.querySelector("#btn-buscar");
+
 function Libro(titulo, autor, anoPublicacion, genero)
 {
     this.titulo = titulo;
@@ -9,35 +11,19 @@ function Libro(titulo, autor, anoPublicacion, genero)
         return `El libro ${this.titulo} fue escrito por ${this.autor} en el año ${this.anoPublicacion} y pertenece al género ${this.genero}`
     }
 }
-let libro1 = new Libro("El señor de los anillos", "Tolkien", 1954, "Fantasía");
-let libro2 = new Libro("El código Da Vinci", "Brown", 2003, "Misterio");
-let libro3 = new Libro("Cien años de soledad", "Márquez", 1967, "Realismo mágico");
-let libro4 = new Libro("El principito", "Saint-Exupéry", 1943, "Infantil");
-let libro5 = new Libro("El alquimista", "Coelho", 1988, "Autoayuda");
-let libro6 = new Libro("El nombre de la rosa", "Eco", 1980, "Misterio");
-let libro7 = new Libro("El retrato de Dorian Gray", "Wilde", 1890, "Drama");
-let libro8 = new Libro("El perfume", "Süskind", 1985, "Novela negra");
-let libro9 = new Libro("La sombra del viento", "Zafón", 2001, "Misterio");
-let libro10 = new Libro("La ladrona de libros", "Zusak", 2005, "Drama");
-let libro11 = new Libro("El guardián entre el centeno", "Zusak", 1951, "Drama");
-let libro12 = new Libro("1984", "Zusak", 1949, "Ciencia ficción");
-let libro13 = new Libro("Rebelión en la granja", "Orwell", 1945, "Fábula");
-
-//escribir los libros en formato json
-console.log(JSON.stringify(libro1));
-console.log(JSON.stringify(libro2));
-console.log(JSON.stringify(libro3));
-console.log(JSON.stringify(libro4));
-console.log(JSON.stringify(libro5));
-console.log(JSON.stringify(libro6));
-console.log(JSON.stringify(libro7));
-console.log(JSON.stringify(libro8));
-console.log(JSON.stringify(libro9));
-console.log(JSON.stringify(libro10));
-console.log(JSON.stringify(libro11));
-console.log(JSON.stringify(libro12));
-console.log(JSON.stringify(libro13));
-
+libro1= new Libro("El señor de los anillos","Tolkien",1954,"Fantasía");
+libro2= new Libro("El código Da Vinci","Brown",2003,"Misterio");
+libro3= new Libro("Cien años de soledad","Márquez",1967,"Realismo mágico");
+libro4= new Libro("El principito","Saint-Exupéry",1943,"Infantil");
+libro5= new Libro("El alquimista","Coelho",1988,"Autoayuda");
+libro6= new Libro("El nombre de la rosa","Eco",1980,"Misterio");
+libro7= new Libro("El retrato de Dorian Gray","Wilde",1890,"Drama");
+libro8= new Libro("El perfume","Süskind",1985,"Novela negra");
+libro9= new Libro("La sombra del viento","Zafón",2001,"Misterio");
+libro10= new Libro("La ladrona de libros","Zusak",2005,"Drama");
+libro11= new Libro("El guardián entre el centeno","Zusak",1951,"Drama");
+libro12= new Libro("1984","Zusak",1949,"Ciencia ficción");
+libro13= new Libro("Rebelión en la granja","Orwell",1945,"Fábula");
 
 let biblioteca = [libro1, libro2, libro3, libro4, libro5, libro6, libro7, libro8, libro9, libro10, libro11, libro12, libro13];
 
@@ -53,7 +39,43 @@ function anadirLibro(titulo, autor, anoPublicacion, genero)
     biblioteca.push(libro);
 }
 
-console.log(buscarLibro("Zusak"));
-anadirLibro("El hobbit", "Tolkien", 1937, "Fantasía");
-console.log(biblioteca);
+botonBuscar.addEventListener("click", function() {
+    let autorIngresado = document.querySelector("#input-autor").value;
+    //controlar que el usuario haya ingresado un autor y no un numero con regex 
+    let regex = /^[a-zA-Z\s]*$/;
+    if (autorIngresado === "")
+    {
+        alert("Ingrese un autor");
+    }
+    else if (regex.test(autorIngresado))
+        {
+            let librosAutor = buscarLibro(autorIngresado);        
+            if (librosAutor.length === 0)
+            {
+                alert("No se encontraron libros de ese autor");
+            }
+            else
+            {
+                //crear un div que contendrá los libros del autor e insertarlo en el DOM
+                let divLibros = document.createElement("div");
+                divLibros.classList.add("libros");
+                document.querySelector("#contenedor-libros").appendChild(divLibros);
+                //iterar sobre los libros del autor y mostrarlos en el div
+                librosAutor.forEach(libro => {
+                    let p = document.createElement("p");
+                    p.textContent = libro.info();
+                    divLibros.appendChild(p);
+                });
+                
+            }
+        }
+    else {
+        alert("Ingrese un autor válido");
+    } 
+     
+
+      
+    
+});
+
 
