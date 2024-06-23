3188f041102af8c90c015d6a583639a4e14a301e
eXe336 <82899@sistemas.frc.utn.edu.ar>
2024-06-23 12:59:07 -0300

    Agregado mejora de eventos y quitadas las alertas

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const botonBuscar = document.querySelector("#btn-buscar");
+let botonBuscar = document.querySelector("#btn-buscar");
+let inputEnter = document.querySelector("#input-autor");
 
 function Libro(titulo, autor, anoPublicacion, genero)
 {
@@ -38,21 +39,31 @@ function anadirLibro(titulo, autor, anoPublicacion, genero)
     let libro = new Libro(titulo, autor, anoPublicacion, genero);
     biblioteca.push(libro);
 }
+let error = document.querySelector("#mensaje");
+//manejador de eventos cuando inputEnter se le da enter con el teclado
+inputEnter.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        botonBuscar.click();
+    }
+});
 
 botonBuscar.addEventListener("click", function() {
+    error.innerHTML = "";
     let autorIngresado = document.querySelector("#input-autor").value;
     //controlar que el usuario haya ingresado un autor y no un numero con regex 
     let regex = /^[a-zA-Z\s]*$/;
     if (autorIngresado === "")
-    {
-        alert("Ingrese un autor");
+    {   let mensaje = "Ingrese un autor"
+        error.innerHTML = mensaje;        
     }
     else if (regex.test(autorIngresado))
         {
             let librosAutor = buscarLibro(autorIngresado);        
             if (librosAutor.length === 0)
             {
-                alert("No se encontraron libros de ese autor");
+                mensaje = "No se encontraron libros de ese autor";
+                error.innerHTML = mensaje;
             }
             else
             {
@@ -70,12 +81,14 @@ botonBuscar.addEventListener("click", function() {
             }
         }
     else {
-        alert("Ingrese un autor válido");
-    } 
-     
-
-      
+        mensaje = "Ingrese un autor válido";
+        error.innerHTML = mensaje;
+    }     
     
 });
+
+anadirLibro("El amor en los tiempos del cólera","Márquez",1985,"Romance");
+anadirLibro("La casa de los espíritus","Allende",1982,"Realismo mágico");
+console.log(biblioteca);
 
 
